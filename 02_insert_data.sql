69a72d54611fc33ed55c6ef1fe9f72530011d8f4
Rafael Sánchez Saucedo <rafaelsanchez9924@gmail.com>
2025-11-12 19:36:04 -0600

    Update data insertion for customers, products, orders, and items
    Inserted new customer, product, order, and order item records while removing old entries.

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,32 +1,25 @@
+INSERT INTO customers (name, email, city, signup_date) VALUES
+('Paty Martinez', 'paty@gmail.com', 'Monterrey', '2023-10-01'),
+('Daniel Sanchez', 'daniel@gmail.com', 'Guadalajara', '2023-10-02'),
+('Rafael Sanchez', 'rafael@gmail.com', 'SLP', '2023-10-03'),
+('Yael Quintanilla', 'yael@gmail.com', 'CDMX', '2023-10-04');
 
--- Insertar la siguiente cantidad de registros
---4 registros en customers
---4 registros en products
---4 registros en orders
---6 registros en order_items
+INSERT INTO products (name, price) VALUES
+('MacBook', 40000.00),
+('Iphone 17 Pro Max', 50000.00),
+('Samsung Galaxy Tab A9', 1999.00),
+('Laptop DELL Inspiron 15', 8900.99);
 
-INSERT INTO customers(name, email, city, signup_date) VALUES 
-        ('Cruz Angel Lopez', 'crunchirroll123@gmail.com', 'Salinas', '10-05-2025'),
-        ('Yamin Guerrero Guevara', 'yazMAX456@gmail.com', 'Soledad de Graciano Sánchez', '9-25-2025'),
-        ('Luis Ángel Vidales Silva', 'angelitobonito@gmail.com', 'Matehuala', '08-15-2025'),
-        ('Ximena Patricia Huerta Alvarado', 'minionas123@gmail.com', 'Tepoztlán', '06-30-2025');
-        
-INSERT INTO products(name, price) VALUES 
-        ('Lapiz', 4.5),
-        ('Goma', 5),
-        ('Sacapuntas', 3.5),
-        ('Pluma', 8);
+INSERT INTO orders (customer_id, order_date) VALUES
+(1, '2023-11-01'),
+(2, '2023-11-03'),
+(3, '2023-11-05'),
+(1, '2023-11-07');
 
-INSERT INTO orders(customer_id, order_date) VALUES 
-        (1, now()),
-        (3, now()),
-        (4, now()),
-        (2, now());
-        
-INSERT INTO order_items(order_id, product_id, quantity) VALUES 
-        (1, 2, 1),
-        (3, 3, 2),
-        (2, 1, 2),
-        (4, 4, 3),
-        (4, 1, 3),
-        (3, 3, 3);
+INSERT INTO order_items (order_id, product_id, quantity) VALUES
+(1, 1, 1),
+(1, 2, 2),
+(2, 3, 1),
+(3, 4, 1),
+(4, 2, 1),
+(4, 4, 1);
